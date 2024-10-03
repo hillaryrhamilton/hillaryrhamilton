649dc87a8a0432105654f5925d6eb82f688390dc
asojk <asojk@users.noreply.github.com>
2024-10-03 00:39:56 +0000

    deploy: af6832e7b8c0d0ac363ab6d003ac815fec0fd524

---
+++ _next/static/chunks/app/projects/page-c65fef4edf65729f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[895,890],{4044:function(n,_,s){Promise.resolve().then(s.t.bind(s,5,23))},5:function(n){n.exports={style:{fontFamily:"'__CalSans_399708', '__CalSans_Fallback_399708'"},className:"__className_399708"}}},function(n){n.O(0,[683,193,268,744],function(){return n(n.s=4044)}),_N_E=n.O()}]);
